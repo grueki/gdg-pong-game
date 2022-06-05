f85205a4fe1359b9bf44f52985e686f28c7daf7b
s-mahnoor-q <105626403+s-mahnoor-q@users.noreply.github.com>
2022-06-05 17:27:45 +1200

    Added Score UI to Display1

--- Assets/Scripts/DebugScore.cs
+++ Assets/Scripts/DebugScore.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreControl : MonoBehaviour
+public class DebugScore : MonoBehaviour
 {
     // Declare variables
-    public int P1Score = 0;
-    public int P2Score = 0;
+    int P1Score = 0;
+    int P2Score = 0;
     
     [SerializeField]
     public Text P1ScoreText;
@@ -16,8 +16,8 @@ public class ScoreControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // P1ScoreText.text = "Player 1 Score: " + P1Score.ToString();
-        // P2ScoreText.text = "Player 1 Score: " + P2Score.ToString();
+        P1ScoreText.text = "Player 1 Score: " + P1Score.ToString();
+        P2ScoreText.text = "Player 2 Score: " + P2Score.ToString();
     }
 
     // Update is called once per frame
@@ -42,7 +42,7 @@ public class ScoreControl : MonoBehaviour
 
             if (name == "Player 2"){
                 P2Score = IncreaseScore(P2Score);
-                P2ScoreText.text = "Player 1 Score: " + P2Score.ToString();
+                P2ScoreText.text = "Player 2 Score: " + P2Score.ToString();
             }
         }
     }
